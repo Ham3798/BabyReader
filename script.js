8ae5296255a3921447664d8ad625c4848df9d93f
Ham3798 <43927226+Ham3798@users.noreply.github.com>
2022-11-28 01:58:51 +0900

    file drop

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
 const nav_links_ChildNodes = document.getElementById("nav_links").getElementsByTagName("a");
+var data = localStorage.getItem("data");
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
+// drag and drop api 예제
+
+function create_book(file) {
+    var book = document.createElement("div");
+    book.textContent = file.name;
+    book.id = data.length;
+    book.href = "./readPage.html";
+    book.className = "m-1 mb-4 bg-light row-lg-2 book";
+
+    document.getElementById("upload").appendChild(book);
+}
+
+function dropHandler(ev) {
+    ev.preventDefault();
+  
+    if (ev.dataTransfer.items) {
+      [...ev.dataTransfer.items].forEach((item, i) => {
+        if (item.kind === 'file') {
+            const file = item.getAsFile();
+            create_book(file);
+            data.push(file);
+        }
+      });
+    } else {
+      [...ev.dataTransfer.files].forEach((file, i) => {
+        console.log(`… file[${i}].name = ${file.name}`);
+      });
+    }
+  }
+  function dragOverHandler(ev) {
+    console.log('File(s) in drop zone');
+  
+    ev.preventDefault();
+  }
+  
 
 
 // nav 영역 클릭시 화면 전환 function
@@ -19,6 +58,17 @@ function move_navs(event) {
     document.getElementById(to.name).style.display = "block";
 }
 
+
+
 for(var i=0;i<nav_links_ChildNodes.length;i++) {
     nav_links_ChildNodes[i].addEventListener("click", move_navs);
+}
+
+if(data == null) {
+    data = Array();
+}
+else {
+    for(var i=0;i<data.length;i++) {
+        data[i];
+    }
 }
